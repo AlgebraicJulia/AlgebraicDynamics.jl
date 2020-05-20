29d975b2ad6b20d81224f22ba2f13dc26fab8501
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2020-05-19 21:07:27 -0400

    Files generated by PkgTemplates

---
+++ src/AlgebraicDynamics.jl
@@ -0,0 +1,5 @@
+module AlgebraicDynamics
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AlgebraicDynamics
+using Test
+
+@testset "AlgebraicDynamics.jl" begin
+    # Write your own tests here.
+end

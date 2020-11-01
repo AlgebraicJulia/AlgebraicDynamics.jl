16a60685e7419fc5bee0e262460300e1fca25a08
Sophie Libkind <sophie.libkind@gmail.com>
2020-11-01 12:01:42 -0800

    EHN: discretization strategy based on ODE solve

--- examples/trajectories.jl
+++ examples/trajectories.jl
@@ -1,4 +1,5 @@
 using DynamicalSystems
+using DifferentialEquations
 using Catlab
 using Catlab.CategoricalAlgebra
 using AlgebraicDynamics.DiscDynam
@@ -26,11 +27,29 @@ ds = Systems.nosehoover(u0)
 xs_ds = Systems.trajectory(ds, T, dt = dt)
 
 # Compositional Euler's Method
-f_eulers = Dynam(eulers(dotf), 2,[1,2], u0[1:2])
-g_eulers = Dynam(eulers(dotg), 2,[1,2], u0[2:3])
+f_eulers = Dynam(eulers(dotf), 2, [1,2], u0[1:2])
+g_eulers = Dynam(eulers(dotg), 2, [1,2], u0[2:3])
 cosp = Cospan(FinFunction([1, 2, 2, 3], 3), FinFunction([1,2,3], 3))
 
 ns_eulers = AlgebraicDynamics.DiscDynam.compose(cosp)(f_eulers, g_eulers)
 
 internal_eulers = AlgebraicDynamics.Trajectory.trajectory(ns_eulers, N, dt)
 xs_eulers = mapslices(x -> get_exposed(ns_eulers, x), internal_eulers, dims = 2)
+
+
+# Compositional best approximation
+function approximate(dotf::Function)
+    u = (x,p,t) -> dotf(x)
+    function step(x0, h)
+        p = ODEProblem(u, x0, (0, h))
+        return solve(p, save_everystep = false, save_start=false,save_end = true)[1]
+    end
+end
+
+
+f_approx = Dynam(approximate(dotf), 2, [1,2], u0[1:2])
+g_approx = Dynam(approximate(dotg), 2, [1,2], u0[2:3])
+ns_approx = AlgebraicDynamics.DiscDynam.compose(cosp)(f_approx, g_approx)
+
+internal_approx = AlgebraicDynamics.Trajectory.trajectory(ns_approx, N, dt)
+xs_approx = mapslices(x -> get_exposed(ns_approx, x), internal_approx, dims = 2)

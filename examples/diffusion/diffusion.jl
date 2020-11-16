d21f187620a5eac88e98e852bc79ea6ae6d29ddc
bosonbaas <bosonbaas@gmail.com>
2020-11-16 10:01:18 -0500

    Added more examples

--- examples/diffusion/diffusion.jl
+++ examples/diffusion/diffusion.jl
@@ -79,3 +79,50 @@ function interp_vals(values::Array{<:Real, 2}, val_to_state::Array{Int,2})
   end
   return res
 end
+
+function initialize_dot(center::Array{<:Real, 1}, std::Array{<:Real, 1}, nesting::Int, Δx::Real)
+  width = 2^nesting
+  # Initialize center values in stencils
+  state = zeros(width, width)
+  dist = Distributions.MvNormal(center, std)
+  for x in 1:width
+    for y in 1:width
+      state[x,y] = pdf(dist, [(x-1)/(width-1), (y-1)/(width-1)])
+    end
+  end
+  # Interpolate port values
+  val_to_state = get_coords(nesting)*6 .- 1
+  values = interp_vals(state, val_to_state, Δx)
+  return values, val_to_state
+end
+
+function interp_vals(values::Array{<:Real, 2}, val_to_state::Array{Int,2}, Δx::Real)
+  width = size(values)[1]
+  res = zeros(width*width*6)
+  res[val_to_state] = values
+  for x in 1:width
+    for y in 1:width
+      cur_ind = val_to_state[x,y] - 4
+      res[cur_ind+5] = (y-1)*Δx
+      res[cur_ind:(cur_ind+3)] .= res[cur_ind+4]
+      #res[cur_ind:(cur_ind+3)] .= 0
+      if x + 1 <= width
+        e_ind = val_to_state[x+1,y]
+        res[cur_ind] = (res[cur_ind] + res[e_ind])/2
+      end
+      if y - 1 > 0
+        s_ind = val_to_state[x,y-1]
+        res[cur_ind+1] = (res[cur_ind+1] + res[s_ind])/2
+      end
+      if x - 1 > 0
+        w_ind = val_to_state[x-1,y]
+        res[cur_ind+2] = (res[cur_ind+2] + res[w_ind])/2
+      end
+      if y + 1 <= width
+        n_ind = val_to_state[x,y+1]
+        res[cur_ind+3] = (res[cur_ind+3] + res[n_ind])/2
+      end
+    end
+  end
+  return res
+end

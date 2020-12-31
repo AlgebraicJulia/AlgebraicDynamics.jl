bdc31d70b4481066557a03c8d3c051b6ccd169da
Sophie Libkind <sophie.libkind@gmail.com>
2020-12-31 10:19:12 -0800

    readout cannot depend on parameters

--- src/machines.jl
+++ src/machines.jl
@@ -5,7 +5,12 @@ using  Catlab.CategoricalAlgebra
 using  Catlab.CategoricalAlgebra.FinSets
 import Catlab.CategoricalAlgebra: coproduct
 import Catlab.WiringDiagrams: oapply
-export AbstractMachine, ContinuousMachine, DiscreteMachine, oapply
+
+export AbstractMachine, ContinuousMachine, DiscreteMachine, 
+nstates, nparams, noutputs, eval_dynamics, readout, euler_approx
+
+using Base.Iterators
+import Base: show, eltype
 
 abstract type AbstractMachine{T} end
 
@@ -27,42 +32,43 @@ end
   
 show(io::IO, vf::ContinuousMachine) = print("ContinuousMachine(ℝ^$(vf.nstates) × ℝ^$(vf.nparams) → ℝ^$(vf.nstates))")
 show(io::IO, vf::DiscreteMachine) = print("DiscreteMachine(ℝ^$(vf.nstates) × ℝ^$(vf.nparams) → ℝ^$(vf.nstates))")
+eltype(m::AbstractMachine{T}) where T = T
 
 nstates(f::AbstractMachine) = f.nstates
 nparams(f::AbstractMachine) = f.nparams
 noutputs(f::AbstractMachine) = f.noutputs
 eval_dynamics(f::AbstractMachine, u, p, args...) = f.dynamics(u,p, args...)
-readout(f::AbstractMachine, u, p, args...) = f.readout(u, p, args...)
+readout(f::AbstractMachine, u, args...) = f.readout(u, args...)
 
 #eulers
-eulers(f::ContinuousMachine{T}, h::Float) where T = DiscreteMachine{T}(
+euler_approx(f::ContinuousMachine{T}, h::Float64) where T = DiscreteMachine{T}(
     nparams(f), nstates(f), noutputs(f), 
     (u, p, args...) -> u + h*eval_dynamics(f, u, p, args...),
     f.readout
 )
 
-eulers(f::ContinuousMachine{T}) where T = DiscreteMachine{T}(
+euler_approx(f::ContinuousMachine{T}) where T = DiscreteMachine{T}(
     nparams(f), nstates(f), noutputs(f), 
     (u, p, h, args...) -> u + h*eval_dynamics(f, u, p, args...),
-    f.readout
+    (u, h, args...) -> f.readout(u, args...)
 )
-
-# trajectories
-# trajectory(f::DiscreteMachine{T}, N::Int, u0::Vector, ps) where T = begin
-#     # it would be good to return a DynamicalSystems dataset
-#     for i in 1:N
-
-#     end
-# end
-
-trajectory(f::DiscreteMachine{T}, N::Int, u0::Vector) where T = begin
-    nparams(f) == 0 || @warn "setting the $(nparams(f)) to zero"
-    trajectory(f, N, u0, zeros(N, nparams(f)))
-end
+euler_approx(fs::Vector{ContinuousMachine{T}}, args...) where T = 
+    map(f->euler_approx(f,args...), fs)
 
 # oapply
+function fills(m::AbstractMachine, d::WiringDiagram, b::Int)
+    b <= nboxes(d) || error("Trying to fill box $b, when $d has fewer than $b boxes")
+    b = box_ids(d)[b]
+    return nparams(m) == length(input_ports(d,b)) && noutputs(m) == length(output_ports(d,b))
+end
+
+
 function oapply(d::WiringDiagram, x::AbstractMachine)
-    oapply(d, collect(repeated(x, nparts(d, :B))))
+    oapply(d, collect(repeated(x, nboxes(d))))
+end
+
+function oapply(d::WiringDiagram, xs::AbstractDict)
+    oapply(d, [xs[box.value] for box in boxes(d)])
 end
 
 colimitmap!(f::Function, output, C::Colimit, input) = begin
@@ -75,61 +81,65 @@ colimitmap!(f::Function, output, C::Colimit, input) = begin
     return output
 end
 
-@inline fillreadouts!(y, d, xs, Outputs, statefun) = colimitmap!(y, Outputs, xs) do i,x
-    return x.readout(statefun(i))
+@inline fillreadouts!(y, d, xs, Outputs, statefun, args...) = colimitmap!(y, Outputs, xs) do i,x
+    return x.readout(statefun(i), args...)
 end
 
-@inline fillstates!(y, d, xs, States, statefun, paramfun) = colimitmap!(y, States, xs) do i, x
-    return x.dynamics(statefun(i), paramfun(i))
+@inline fillstates!(y, d, xs, States, statefun, paramfun, args...) = colimitmap!(y, States, xs) do i, x
+    return x.dynamics(statefun(i), paramfun(i), args...)
 end
 
-@inline fillwire(w, d, readouts, Outputs, p) = begin
-    if w.source.box == input_id(d)
-        return p[w.source.port]
-    end
-    return readouts[legs(Outputs)[w.source.box - 2](w.source.port)] # FIX - box re-indexing
-end
+@inline fillwire(w, d, readouts, Outputs) = readouts[legs(Outputs)[w.source.box - 2](w.source.port)] # FIX - box re-indexing
 
 fillreadins!(readins, d, readouts, Outputs, Params, p) = begin
     for (i,w) in enumerate(wires(d))
         if w.target.box == output_id(d)
             continue
+        elseif w.source.box == input_id(d)
+            readins[legs(Params)[w.target.box - 2](w.target.port)] += p[w.source.port]
+        else
+            readins[legs(Params)[w.target.box - 2](w.target.port)] += fillwire(w, d, readouts, Outputs)
         end
-        readins[legs(Params)[w.target.box - 2](w.target.port)] = fillwire(w, d, readouts, Outputs, p)
+        
     end
     return readins
 end
 
 function oapply(d::WiringDiagram, xs::Vector{Machine}) where {T, Machine <: AbstractMachine{T}}
-    #nboxes(composite) == length(dynamics)  || error("there are $nboxes(composite) boxes but $length(dynamics) machines")
+    isempty(wires(d, input_id(d), output_id(d))) || error("d is not a valid composition syntax because it has pass wires")
+    nboxes(d) == length(xs)  || error("there are $nboxes(d) boxes but $length(xs) machines")
+    for box in 1:nboxes(d)
+        fills(xs[box], d, box) || error("$xs[box] does not fill box $box")
+    end
 
     S = coproduct((FinSet∘nstates).(xs))
-
     Params = coproduct((FinSet∘nparams).(xs))
     Outputs = coproduct((FinSet∘noutputs).(xs))
-
-    readouts = zeros(T, length(apex(Outputs)))
-    readins = zeros(T, length(apex(Params)))
-
     ys = zeros(T, length(apex(S)))
+
     states(u::Vector, b::Int) = u[legs(S)[b](1:xs[b].nstates)]
 
+    v = (u::AbstractVector, p::AbstractVector, args...) -> begin
+        readouts = zeros(T, length(apex(Outputs)))
+        readins = zeros(T, length(apex(Params)))
 
-    v = (u::AbstractVector, p::AbstractVector, t::Real=0.0) -> begin
         get_states(b) = states(u,b)
         get_params(b) = view(readins, legs(Params)[b](:))
-        fillreadouts!(readouts, d, xs, Outputs, get_states)
+        
+        fillreadouts!(readouts, d, xs, Outputs, get_states, args...)
         fillreadins!(readins, d, readouts, Outputs, Params, p)
-        fillstates!(ys, d, xs, S, get_states, get_params)
+        fillstates!(ys, d, xs, S, get_states, get_params, args...)
         return ys
     end
 
-    function readout(u::AbstractVector, p::AbstractVector, t::Real=0.0)
+    function readout(u::AbstractVector, args...)
+        readouts = zeros(T, length(apex(Outputs)))
         get_states(b) = states(u,b)
-        fillreadouts!(readouts, d, xs, Outputs, get_states)
+
+        fillreadouts!(readouts, d, xs, Outputs, get_states, args...)
         r = zeros(T, length(d.output_ports))
         for w in in_wires(d, output_id(d))
-            r[w.target.port] += fillwire(w, d, readouts, Outputs, p)
+            r[w.target.port] += fillwire(w, d, readouts, Outputs)
         end
         return r
     end
--- test/machines.jl
+++ test/machines.jl
@@ -10,29 +10,34 @@ rf(x) = x
 mf = ContinuousMachine{Float64}(1,1,1, uf, rf)
 
 f = Box(:f, A, A)
-m_id = oapply(singleton_diagram(f), [mf])
+d_id = singleton_diagram(f)
+m_id = oapply(d_id, [mf])
 
 x0 = 1
 p0 = 200
 @test m_id.dynamics([x0], [p0]) == [p0 - x0]
-@test m_id.readout([x0], [p0]) == [x0]
+@test m_id.readout([x0]) == [x0]
+
+# unfed parameter 
+d12 = WiringDiagram(A, A)
+f1 = add_box!(d12, f)
+f2 = add_box!(d12, f)
+add_wires!(d12, Pair[
+    (f1, 1) => (f2, 1),
+    (f2, 1) => (output_id(d12), 1)
+])
+m12 = oapply(d12, mf)
+
 
 # composite
-d = WiringDiagram(A, A)
-f1 = add_box!(d, f)
-f2 = add_box!(d, f)
-add_wires!(d, Pair[
-    (input_id(d), 1) => (f1, 1),
-    (f1, 1) => (f2, 1),
-    (f2, 1) => (output_id(d), 1)
-])
-m12 = oapply(d, [mf, mf])
+add_wire!(d12, (input_id(d12), 1) => (f1, 1))
+m12 = oapply(d12, [mf, mf])
 
 x0 = -1
 y0 = 29
 p0 = 200
 @test m12.dynamics([x0, y0], [p0]) == [p0 - x0, x0 - y0]
-@test m12.readout([x0,y0], [p0]) == [y0]
+@test m12.readout([x0,y0]) == [y0]
 
 
 # break and back together
@@ -45,15 +50,95 @@ add_wires!(d, Pair[
     (f1, 1) => (output_id(d), 1),
     (f2, 1) => (output_id(d), 1)
 ])
-m = oapply(d, [mf, mf])
+m = oapply(d, Dict(:f => mf))
 
 x0 = -1
 y0 = 29
 p0 = 200
 @test m.dynamics([x0, y0], [p0]) == [p0 - x0, p0 - y0]
-@test m.readout([x0,y0], [p0]) == [x0 + y0]
+@test m.readout([x0,y0]) == [x0 + y0]
 
 @test m.nparams == 1
 @test m.nstates == 2
 @test m.noutputs == 1
+
+# trace
+d_trace = WiringDiagram(A, A)
+f1 = add_box!(d_trace, f)
+add_wires!(d_trace, Pair[
+    (input_id(d_trace), 1) => (f1, 1),
+    (f1, 1) => (f1, 1),
+    (f1, 1) => (output_id(d_trace), 1)
+])
+m_trace = oapply(d_trace, mf)
+@test nstates(m_trace) == 1
+@test eval_dynamics(m_trace, [x0], [p0]) == [p0]
+@test readout(m_trace, [x0]) == [x0]
+
+
+# oapply and ocompose
+d_tot = ocompose(d, [d12, d_id])
+m_tot1 = oapply(d_tot, mf) 
+m_tot2 = oapply(d, [m12, m_id])
+x0 = [-1, 5.5, 20]
+@test nstates(m_tot1) == 3
+@test nstates(m_tot2) == 3
+
+@test eval_dynamics(m_tot1, x0, [p0]) == eval_dynamics(m_tot2, x0, [p0])
+@test eval_dynamics(m_tot1, x0, [p0]) == [p0 - x0[1], x0[1] - x0[2], p0 - x0[3]]
+
+@test readout(m_tot1, x0) == [x0[2] + x0[3]] 
+@test readout(m_tot2, x0) == [x0[2] + x0[3]] 
+
+# big diagram
+d = WiringDiagram([:A, :A], [:A, :A, :A])
+b1 = add_box!(d, Box(:f, [:A, :A], [:A]))
+b2 = add_box!(d, Box(:g, [:A], [:A, :A]))
+b3 = add_box!(d, Box(:h, [:A], [:A]))
+
+bin = input_id(d); bout = output_id(d)
+
+add_wires!(d, Pair[
+    (bin, 1) => (b1, 2),
+    (bin, 1) => (b2, 1),
+    (bin, 2) => (b3, 1),
+    (b1, 1) => (bout, 1), 
+    (b2, 1) => (bout, 1),
+    (b2, 1) => (bout, 2),
+    (b2, 2) => (b2, 1),
+    (b3, 1) => (b2, 1),
+    (b3, 1) => (bout, 3)
+])
+
+m1 = ContinuousMachine{Float64}(2,1,1, 
+        (x, p) -> [p[1] * p[2]  - x[1]], 
+        x -> 2*x)
+m2 = ContinuousMachine{Float64}(1, 2, 2, 
+        (x, p) -> [x[1]*x[2], p[1]^2*x[2]], 
+        x -> x)
+
+m3 = ContinuousMachine{Float64}(1,2,1, 
+        (x, p) -> [x[1]^2 - p[1], x[2] - x[1]], 
+        x -> [x[1] + x[2]])
+
+xs = Dict(:f => m1, :g => m2, :h => m3, :j => mf)
+m = oapply(d, xs)
+@test nparams(m) == 2
+@test nstates(m) == 5
+@test noutputs(m) == 3
+x = [2.0, 3.0, 5.0, -7.0, -0.5]
+p = [0.1, 11.0]
+@test eval_dynamics(m, x, p) == 
+    [-x[1], x[2]*x[3], (p[1]+x[3]+x[4]+x[5])^2*x[3], x[4]^2 - p[2], x[5] - x[4]]
+@test readout(m, x) == [2*x[1] + x[2], x[2], x[4] + x[5]]
+@test readout(oapply(d_id, m3), [x[4], x[5]]) == [x[4] + x[5]]
+
+# eulers
+h = 0.15
+euler_m = oapply(d, euler_approx([m1, m2, m3], h))
+@test eval_dynamics(euler_m, x, p) == x + h*eval_dynamics(m, x, p)
+@test eval_dynamics(euler_m, x, p) == eval_dynamics(euler_approx(m, h), x, p)
+euler_m2 = oapply(d, euler_approx([m1, m2, m3]))
+@test eval_dynamics(euler_m, x, p) == eval_dynamics(euler_m2, x, p, h)
+
 end

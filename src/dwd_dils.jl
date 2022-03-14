03fb03af684f814c7b2922b1f0ec574e2dfcd022
Sophie Libkind <4992461+slibkind@users.noreply.github.com>
2022-03-14 15:31:19 -0700

    Added doc strings and braiding

--- src/dwd_dils.jl
+++ src/dwd_dils.jl
@@ -3,28 +3,66 @@ using LinearAlgebra
 using Test
 using Plots: plot
 
+""" 
 
+A monomial is a pair of integers  A => B representing the poly ``\mathbb{R}^By^{\mathbb{R}^A}``
+"""
 const Monomial = Pair{Int,Int}
+tensor(ms::Vector{Monomial})::Monomial = sum(first.(ms)) => sum(last.(ms))
+
+positions(m::Monomial) = m.second
+directions(m::Monomial) = m.first
+positions(ms::Vector{Monomial}) = sum(first.(ms))
 
 
 struct DILS{T}
-  input_poly::Monomial
-  output_poly::Monomial
+  input_polys::Vector{Monomial}
+  output_polys::Vector{Monomial}
   nstates::Int # dimension of the state space
   forward_readout::Function
   backward_readout::Function
   eval_dynamics::Function
 end
 
+DILS{T}(input_poly::Monomial, output_poly::Monomial, nstates::Int, 
+        forward_readout::Function, backward_readout::Function, eval_dynamics::Function) where T = 
+  DILS{T}([input_poly], [output_poly], nstates, forward_readout, backward_readout, eval_dynamics)
 
-input_poly(d::DILS) = d.input_poly
-output_poly(d::DILS) = d.output_poly
+
+
+input_polys(d::DILS) = d.input_polys
+output_polys(d::DILS) = d.output_polys
+input_poly(d::DILS) = tensor(input_polys(d))
+output_poly(d::DILS) = tensor(output_polys(d))
+
 nstates(d::DILS) = d.nstates
+
+""" forward_readout(d::DILS, u::AbstractVector, x::AbstractVector)
+
+This is half of the map on positions for the DILS `d` given by ``Sy^S \to [p,q]``. It takes a state ``u \in \mathbb{R}^S``
+and a position in the input polynomial ``x \in p(1)`` and returns a position of the output polynomial.
+"""
 
 forward_readout(d::DILS, u::AbstractVector, x::AbstractVector) =
   d.forward_readout(u, x)
+  
+""" backward_readout(d::DILS, u::AbstractVector, x::AbstractVector, y::AbstractVector)
+
+This is the other half of the map on positions. It takes a state, a position on the input polynomial 
+and a position on the output polynomial and returns a direction on the input polynomial.
+``\sum_{s \in S} \sum_{i\in p(1)} q[f(s,i)] \rightarrow p[i]``
+"""
+
 backward_readout(d::DILS, u::AbstractVector, x::AbstractVector, y::AbstractVector) =
   d.backward_readout(u, x, y)
+
+""" eval_dynamics(d::DILS, u::AbstractVector, x::AbstractVector, y::AbstractVector)
+
+This is the map on directions of the DILS `d` given by ``{\mathbb{R}^S}y^{\mathbb{R}^S} \to [p,q]`. It takes a state 
+``u \in \mathbb{R}^S``, a position ``x \in p(1)``, a direction ``y \in q[f(u,x)]`` (where ``f`` is the `forward_readout`)
+and returns an updated state in ``\mathbb{R}^S``
+"""
+
 eval_dynamics(d::DILS, u::AbstractVector, x::AbstractVector, y::AbstractVector) =
   d.eval_dynamics(u, x, y)
 
@@ -32,46 +70,56 @@ isa_state(d::DILS{T}, u::AbstractVector{T}) where T =
   length(u) == nstates(d)
 
 
+# A DILS based on a function
 function DILS{T}(f::Function, ninput::Int, noutput::Int) where T
   DILS{T}(
-    0=>ninput,
-    0=>noutput,
-    0,
-    (_, x) -> f(x)),  #forward map
+    0=>ninput,  # input poly is ninput y^0
+    0=>noutput, # input poly is nouput y^0
+    0,          # trivial state
+    (_, x) -> f(x),  #forward map
     (_, _, _) -> T[], # backward map
     (_, _, _) -> T[]  # eval dynamics
+  )
 end
 
+# A DILS that outputs constant values
+DILS{T}(c::AbstractVector) where T = DILS{T}(_ -> c, 0, length(c))
 
 """
-forward(x)::
-backward(x,y)
+Basically a (dependent) lens
 """
 function DILS{T}(forward::Function, backward::Function, ninput::Int, noutput::Int) where T
   DILS{T}(
-    ninput=>ninput,
-    noutput=>noutput,
-    0,
+    [ninput=>ninput],
+    [noutput=>noutput],
+    0,                     # trivial state
     (_, x) -> forward(x),  #forward map
     (_, x, y) -> backward(x,y), # backward map
     (_, _, _) -> T[]  # eval dynamics
     )
 end
 
+# A DILS with 1 -> [Ry^R,Ry^R] representing "relu" with gradient descent
 relu = DILS{Float64}(
-    (x) -> [max(0, x[1])],
-    (x,y)-> x[1] < 0 ? [0] : y,
+    (x) -> [max(0, x[1])],          # relu
+    (x,y)-> x[1] < 0 ? [0] : y,     # gradient of relu
     1, 1
 )
 
 @test forward_readout(relu, [], [-1]) == [0]
+@test forward_readout(relu, [], [1]) == [1]
+@test backward_readout(relu, [], [10], [1]) == [1]
+@test backward_readout(relu, [], [-10], [1]) == [0]
 
-"""Gradient descent dynamics"""
+"""Gradient descent dynamics
+
+An unactivated_neuron with n inputs is a DILS R^{n+1} y^{R^{n+1}} -> [R^n y^{R^n}, Ry^R]
+"""
 function unactivated_neuron(n_inputs::Int)
   DILS{Float64}(
-    n_inputs => n_inputs, # input
-    1 => 1, # outputs
-    n_inputs+1, # state (bias is last element)
+    [n_inputs => n_inputs],           # input
+    [1 => 1],                         # outputs
+    n_inputs+1,                       # state (bias is last element)
     (u, x) -> [dot(u, vcat(x,[1]))],  #forward readout
     (u, x, Δy) -> Δy .* u[1:end-1],          # backward readout - returns Δx
     (u, x, Δy) -> Δy .* vcat(x, [1])         # eval_dynamics - returns Δu
@@ -79,27 +127,46 @@ function unactivated_neuron(n_inputs::Int)
 end
 
 
+
 """
 Compose two DILS in sequences
+
+TODO: Change braiding to be any wiring pattern from d1 to d2. 
+If the output poly and input poly are of the form Ay^A (i.e. same inputs and outputs) then 
+you can do this by constructing an integer-valued matrix based on the wiring pattern encoding copying, merging, delete, and create. 
+For the forward pass multiply the positions of the output of d1 by the matrix. 
+For the backward pass, multiply the directions of the input of d2 by the transpose of the matrix.
 """
-function sequential_compose(d1::DILS{T}, d2::DILS{T})::DILS{T} where T
+function sequential_compose(d1::DILS{T}, d2::DILS{T}, braiding::Union{Nothing,AbstractVector} = nothing)::DILS{T} where T
+  # @assert Base.isperm(braiding)
+  #assert positions(output_poly(d1)) == positions(input_poly(d2)) # TODO: check directions match
+  braiding = isnothing(braiding) ? (1:positions(output_poly(d1))) : braiding
   d1_state = u -> u[1:d1.nstates]
   d2_state = u -> u[d1.nstates+1:end]
   d1_output = (u,x) -> forward_readout(d1, d1_state(u), x)
   d2_backward = (u,x,y) -> backward_readout(d2, d2_state(u), d1_output(u,x), y)
   return DILS{T}(
-    input_poly(d1), # input monomial
-    output_poly(d2), # output monomial
+    input_polys(d1), # input monomials
+    output_polys(d2), # output monomials
     nstates(d1)+nstates(d2), # number of states/dimensions
-    (u,x) -> forward_readout(d2, d2_state(u), d1_output(u,x)),
-    (u,x,y) -> backward_readout(d1, d1_state(u), x, d2_backward(u,x,y)),
-    (u,x,y) -> vcat(eval_dynamics(d1, d1_state(u), x, d2_backward(u, x, y)), # updated state for d1
-                    eval_dynamics(d2, d2_state(u), d1_output(u,x), y))       # updated state for d2
+    (u,x) -> forward_readout(d2, d2_state(u), d1_output(u,x)[braiding]),
+    (u,x,y) -> backward_readout(d1, d1_state(u), x, d2_backward(u,x,y)[invperm(braiding)]),
+    (u,x,y) -> vcat(eval_dynamics(d1, d1_state(u), x, d2_backward(u, x, y)[invperm(braiding)]), # updated state for d1
+                    eval_dynamics(d2, d2_state(u), d1_output(u,x)[braiding], y))       # updated state for d2
   )
 end
 
-activated_neuron(ninputs::Int) = sequential_compose(unactivated_neuron(ninputs), relu)
 
+minus = DILS{Float64}(x->[x[1] - x[2]], 2, 1)
+
+constant = DILS{Float64}([1,2])
+
+one_minus_two = sequential_compose(constant, minus)
+two_minus_one = sequential_compose(constant, minus, [2,1])
+forward_readout(one_minus_two, [], []) == [-1]
+forward_readout(two_minus_one, [], []) == [1]
+
+activated_neuron(ninputs::Int) = sequential_compose(unactivated_neuron(ninputs), relu)
 
 
 
@@ -159,8 +226,57 @@ plot(1:length(nonzero_errs), nonzero_errs)
 
 plot(first.(data), last.(data))
 
+"""TODO TEST"""
+function unit(::DILS{T})
+  DILS{T}(Monomial[],Monomial[], 0, id, id, id)
+end
+
+function tensor(d1::DILS{T}, d2::DILS{T}) where T
+  d1_state = u -> u[1:d1.nstates]
+  d2_state = u -> u[d1.nstates+1:end]
+  DILS{T}(tensor(input_polys(d1), input_polys(d2)), 
+          tensor(output_polys(d1), output_polys(d2)),
+          d1.nstates + d2.nstates, 
+          (u,x) -> vcat(forward_readout(d1, u[1:d1.nstates], x[1:first(input_poly(d1))], forward_readout(d2, )))
+  )
+end
+
+"""TODO TEST"""
+tensor(ds::Vector(DILS{T})) where {T} = reduce(tensor, ds, unit(DILS{T}}))
 
 function oapply(d::WiringDiagram, dils::Vector{DILS})::DILS
+  # check that dils[i] "fills" box i of d
+  @assert length(dils) == nboxes(d)
+  for (dil, box) in zip(dils, boxes(d))
+    @assert length(input_ports(d, box)) == length(input_polys(dil))
+    @assert length(input_ports(d, box)) == length(output_polys(dil))
+  end
+
+  layers = layerize(internal_graph(d))
+
+  t_layers = [tensor(dils[i] for i in layer) for layer in layers]
+  return reduce(compose, t_layers)
+end
+
+
+"""
+Partition vertices of a graph by dependency. Get ordered layers.
+Can be used for composing morphisms in a SMC.
+"""
+function layerize(G::Graph)::Vector{Vector{Int}}
+  seen = Set{Int}()
+  layers = Vector{Int}[]
+  while length(seen) < nv(G)
+    next_layer = Int[]
+    for v in filter(x->x∉ seen, parts(G, :V))
+      if G[incident(G, v, :tgt) , :src] ⊆ seen
+        push!(next_layer, v)
+      end
+    end
+    push!(layers, next_layer)
+    union!(seen, next_layer)
+  end
+  return layers
 end
 
 

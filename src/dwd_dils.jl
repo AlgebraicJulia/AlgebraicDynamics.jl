88c33fd4843d64235ec59c178008e9d651264d49
Sophie Libkind <4992461+slibkind@users.noreply.github.com>
2022-03-21 20:42:44 -0700

    sequential composition

--- src/dwd_dils.jl
+++ src/dwd_dils.jl
@@ -1,4 +1,6 @@
 using Catlab.WiringDiagrams.DirectedWiringDiagrams
+using Catlab.Graphs, Catlab.CategoricalAlgebra
+
 using LinearAlgebra
 using Test
 using Plots: plot
@@ -7,8 +9,9 @@ using Plots: plot
 
 A monomial is a pair of integers  A => B representing the poly ``\mathbb{R}^By^{\mathbb{R}^A}``
 """
+
 const Monomial = Pair{Int,Int}
-tensor(ms::Vector{Monomial})::Monomial = sum(first.(ms)) => sum(last.(ms))
+tensor(ms::Vector{Monomial}, ns::Vector{Monomial}) = sum(first.(ms)) => sum(last.(ms))
 
 positions(m::Monomial) = m.second
 directions(m::Monomial) = m.first
@@ -71,10 +74,10 @@ isa_state(d::DILS{T}, u::AbstractVector{T}) where T =
 
 
 # A DILS based on a function
-function DILS{T}(f::Function, ninput::Int, noutput::Int) where T
+function DILS{T}(f::Function, ninput::Int, noutput::Int; monomial=true) where T
   DILS{T}(
-    0=>ninput,  # input poly is ninput y^0
-    0=>noutput, # input poly is nouput y^0
+    monomial ? 0=>ninput : [0=>1 for _ in 1:ninput],  # input poly is ninput y^0
+    monomial ? 0=>noutput : [0=>1 for _ in 1:noutput], # input poly is nouput y^0
     0,          # trivial state
     (_, x) -> f(x),  #forward map
     (_, _, _) -> T[], # backward map
@@ -83,7 +86,7 @@ function DILS{T}(f::Function, ninput::Int, noutput::Int) where T
 end
 
 # A DILS that outputs constant values
-DILS{T}(c::AbstractVector) where T = DILS{T}(_ -> c, 0, length(c))
+DILS{T}(c::AbstractVector; monomial=true) where T = DILS{T}(_ -> c, 0, length(c); monomial=monomial)
 
 """
 Basically a (dependent) lens
@@ -137,34 +140,68 @@ you can do this by constructing an integer-valued matrix based on the wiring pat
 For the forward pass multiply the positions of the output of d1 by the matrix. 
 For the backward pass, multiply the directions of the input of d2 by the transpose of the matrix.
 """
-function sequential_compose(d1::DILS{T}, d2::DILS{T}, braiding::Union{Nothing,AbstractVector} = nothing)::DILS{T} where T
-  # @assert Base.isperm(braiding)
-  #assert positions(output_poly(d1)) == positions(input_poly(d2)) # TODO: check directions match
-  braiding = isnothing(braiding) ? (1:positions(output_poly(d1))) : braiding
+
+# function sequential_compose(d1::DILS{T}, d2::DILS{T}, braiding::Nothing)::DILS{T} where T 
+#   sequential_compose(d1::DILS{T})
+# end
+
+input_poly(d::DILS, i::Int) = input_polys(d)[i]
+output_poly(d::DILS, i::Int) = output_polys(d)[i]
+
+function sequential_compose(d1::DILS{T}, d2::DILS{T}, weights::AbstractMatrix)::DILS{T} where T
+  # check that the monomials match
+  for (i,k) in pairs(weights) 
+    if k > 0 
+      input_poly(d2, i[1]) == output_poly(d1, i[2])
+    end
+  end
   d1_state = u -> u[1:d1.nstates]
   d2_state = u -> u[d1.nstates+1:end]
   d1_output = (u,x) -> forward_readout(d1, d1_state(u), x)
   d2_backward = (u,x,y) -> backward_readout(d2, d2_state(u), d1_output(u,x), y)
+
+  # This produces the matrices that copy and add the things flowing along the wires correctly.
+  forward_matrix = generalized_kroenecker(weights, map(positions, input_polys(d2)), map(positions, output_polys(d1)))
+  backward_matrix = generalized_kroenecker(weights', map(directions, output_polys(d1)), map(directions, input_polys(d2)))
   return DILS{T}(
     input_polys(d1), # input monomials
     output_polys(d2), # output monomials
     nstates(d1)+nstates(d2), # number of states/dimensions
-    (u,x) -> forward_readout(d2, d2_state(u), d1_output(u,x)[braiding]),
-    (u,x,y) -> backward_readout(d1, d1_state(u), x, d2_backward(u,x,y)[invperm(braiding)]),
-    (u,x,y) -> vcat(eval_dynamics(d1, d1_state(u), x, d2_backward(u, x, y)[invperm(braiding)]), # updated state for d1
-                    eval_dynamics(d2, d2_state(u), d1_output(u,x)[braiding], y))       # updated state for d2
+    (u,x) -> forward_readout(d2, d2_state(u), forward_matrix*d1_output(u,x)),
+    (u,x,y) -> backward_readout(d1, d1_state(u), x, backward_matrix*d2_backward(u,x,y)),
+    (u,x,y) -> vcat(eval_dynamics(d1, d1_state(u), x, backward_matrix'*d2_backward(u, x, y)), # updated state for d1
+                    eval_dynamics(d2, d2_state(u), forward_matrix*d1_output(u,x), y))       # updated state for d2
   )
 end
 
 
-minus = DILS{Float64}(x->[x[1] - x[2]], 2, 1)
+function generalized_kroenecker(weights::AbstractMatrix, rowdims::Vector{Int}, coldims::Vector{Int})
+  
+  block = map(enumerate(coldims)) do (col, coldim)
+            vcat(map(enumerate(rowdims)) do (row, rowdim)
+               weights[row,col] * (rowdim == coldim ? I(rowdim) : zeros(rowdim, coldim)) 
+            end...)
+          end
+  return hcat(block...)
+end
 
-constant = DILS{Float64}([1,2])
+generalized_kroenecker([1 0; 1 0; 0 1], [3,3,2],[3, 2])
 
-one_minus_two = sequential_compose(constant, minus)
-two_minus_one = sequential_compose(constant, minus, [2,1])
+minus = DILS{Float64}(x->[x[1] - x[2]], 2, 1; monomial=false)
+@assert length(input_polys(minus)) == 2
+
+constant = DILS{Float64}([1,2]; monomial=false)
+
+one_minus_two = sequential_compose(constant, minus, [1 0; 0 1])
+two_minus_one = sequential_compose(constant, minus, [0 1; 1 0])
+
+two_minus_two = sequential_compose(constant, minus, [0 1; 2 0])
+
 forward_readout(one_minus_two, [], []) == [-1]
 forward_readout(two_minus_one, [], []) == [1]
+forward_readout(two_minus_two, [], []) == [0]
+
+backward_readout(one_minus_two, [], [], []) == []
 
 activated_neuron(ninputs::Int) = sequential_compose(unactivated_neuron(ninputs), relu)
 
@@ -227,22 +264,22 @@ plot(1:length(nonzero_errs), nonzero_errs)
 plot(first.(data), last.(data))
 
 """TODO TEST"""
-function unit(::DILS{T})
+function unit(::DILS{T}) where T
   DILS{T}(Monomial[],Monomial[], 0, id, id, id)
 end
 
 function tensor(d1::DILS{T}, d2::DILS{T}) where T
   d1_state = u -> u[1:d1.nstates]
   d2_state = u -> u[d1.nstates+1:end]
-  DILS{T}(tensor(input_polys(d1), input_polys(d2)), 
-          tensor(output_polys(d1), output_polys(d2)),
+  DILS{T}(vcat(input_polys(d1), input_polys(d2)), 
+          vcat(output_polys(d1), output_polys(d2)),
           d1.nstates + d2.nstates, 
-          (u,x) -> vcat(forward_readout(d1, u[1:d1.nstates], x[1:first(input_poly(d1))], forward_readout(d2, )))
+          (u,x) -> vcat(forward_readout(d1, d1_state(u), x[1:positions(input_poly(d1))], forward_readout(d2, d2_state(u), x[positions(input_poly(d1))+1,end])))
   )
 end
 
 """TODO TEST"""
-tensor(ds::Vector(DILS{T})) where {T} = reduce(tensor, ds, unit(DILS{T}}))
+tensor(ds::Vector{D}) where {T, D<:DILS{T}} = reduce(tensor, ds, unit(DILS{T}))
 
 function oapply(d::WiringDiagram, dils::Vector{DILS})::DILS
   # check that dils[i] "fills" box i of d
@@ -255,15 +292,41 @@ function oapply(d::WiringDiagram, dils::Vector{DILS})::DILS
   layers = layerize(internal_graph(d))
 
   t_layers = [tensor(dils[i] for i in layer) for layer in layers]
-  return reduce(compose, t_layers)
+  res = t_layers[1]
+  for (i, layer) in t_layers[2:end]
+    weights = map(in_ports(d, ))
+    res = compose(res, layer, weights) 
+  end
+  return res 
+
+d = WiringDiagram([], [:X])
+b1 = add_box!(d, Box(:const, [], [:one, :two]))
+b2 = add_box!(d, Box(:minus, [:x, :y], [:x_minus_y]))
+add_wires!(d, Pair[
+  (b1, 1) => (b2, 2),
+  (b1, 2) => (b2, 1),
+  (b1, 1) => (b2, 1),
+  (b2, 1) => (output_id(d), 1)
+])
+
+g = internal_graph(d)
+layers = layerize(internal_graph(d))
+
+dils = [constant, minus]
+
+for (dil, box) in zip(dils, 1:nboxes(d))
+  @assert length(input_ports(d, box)) == length(input_polys(dil)) 
+  @assert length(output_ports(d, box)) == length(output_polys(dil))
 end
 
+
+oapply(d, dils)
 
 """
 Partition vertices of a graph by dependency. Get ordered layers.
 Can be used for composing morphisms in a SMC.
 """
-function layerize(G::Graph)::Vector{Vector{Int}}
+function layerize(G::AbstractGraph)::Vector{Vector{Int}}
   seen = Set{Int}()
   layers = Vector{Int}[]
   while length(seen) < nv(G)

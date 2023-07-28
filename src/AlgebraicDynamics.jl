6166c8adce23d989e707d047d71d984f2236826f
Evan Patterson <evan@epatters.org>
2023-07-28 15:20:31 -0700

    CLEANUP: Reexport top-level modules for user's convenience.

--- src/AlgebraicDynamics.jl
+++ src/AlgebraicDynamics.jl
@@ -1,9 +1,13 @@
 module AlgebraicDynamics
 
-using Catlab
+using Reexport
 
 include("uwd_dynam.jl")
 include("dwd_dynam.jl")
 include("cpg_dynam.jl")
+
+@reexport using .DWDDynam
+@reexport using .UWDDynam
+@reexport using .CPortGraphDynam
 
 end # module

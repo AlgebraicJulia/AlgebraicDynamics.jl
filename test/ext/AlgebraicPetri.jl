649bd835684619754a0907595e8f26b9e063b49b
James P. Fairbanks <fairbanksj@ufl.edu>
2024-06-07 21:02:59 -0400

    TST: fix AlgebraicPetri whitespace

--- test/ext/AlgebraicPetri.jl
+++ test/ext/AlgebraicPetri.jl
@@ -6,33 +6,35 @@ using AlgebraicDynamics.UWDDynam
 using LabelledArrays
 
 
-@testset "AlgebraicPetri" begin 
-  birth_petri = Open(PetriNet(1, 1=>(1,1)));
+@testset "AlgebraicPetri" begin
+  birth_petri = Open(PetriNet(1, 1 => (1, 1)))
   rs = ContinuousResourceSharer{Float64}(birth_petri)
   @test nstates(rs) == 1
   @test nports(rs) == 1
   @test portmap(rs) == [1]
-  u = [1.0]; p = [2.0];
+  u = [1.0]
+  p = [2.0]
   @test eval_dynamics(rs, u, p, 0) == [2.0]
 
-  labelled_birth_petri = Open(LabelledPetriNet([:foo], (:bar,(:foo=>(:foo,:foo)))));
+  labelled_birth_petri = Open(LabelledPetriNet([:foo], (:bar, (:foo => (:foo, :foo)))))
   rs = ContinuousResourceSharer{Float64}(labelled_birth_petri)
   @test nstates(rs) == 1
   @test nports(rs) == 1
   @test portmap(rs) == [1]
-  u = LVector(foo=1.0); p = LVector(bar=2.0);
+  u = LVector(foo=1.0)
+  p = LVector(bar=2.0)
   @test eval_dynamics(rs, u, p, 0) == [2.0]
 
-  labelled_birth_react = Open(LabelledReactionNet{Float64}{Float64}([:foo=>1.0], ((:bar=>2.0),(:foo=>(:foo,:foo)))));
+  labelled_birth_react = Open(LabelledReactionNet{Float64}{Float64}([:foo => 1.0], ((:bar => 2.0), (:foo => (:foo, :foo)))))
   rs = ContinuousResourceSharer{Float64}(labelled_birth_react)
   @test nstates(rs) == 1
   @test nports(rs) == 1
   @test portmap(rs) == [1]
-  u = LVector(NamedTuple(zip(snames(apex(labelled_birth_react)),Vector(subpart(apex(labelled_birth_react),:concentration)))))
+  u = LVector(NamedTuple(zip(snames(apex(labelled_birth_react)), Vector(subpart(apex(labelled_birth_react), :concentration)))))
   p = LVector(bar=2.0)
   @test eval_dynamics(rs, u, p, 0) == [2.0]
 
-  Brusselator= LabelledPetriNet([:A, :B, :D, :E, :X, :Y], 
+  Brusselator = LabelledPetriNet([:A, :B, :D, :E, :X, :Y],
     :t1 => (:A => (:X, :A)),
     :t2 => ((:X, :X, :Y) => (:X, :X, :X)),
     :t3 => ((:B, :X) => (:Y, :D, :B)),
@@ -43,5 +45,5 @@ using LabelledArrays
   rs = ContinuousResourceSharer{Float64}(open_bruss)
   @test nstates(rs) == 6
   @test nports(rs) == 4
-  @test portmap(rs) == [1,3, 1,2]
+  @test portmap(rs) == [1, 3, 1, 2]
 end

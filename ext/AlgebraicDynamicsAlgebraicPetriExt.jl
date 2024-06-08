e5bf2bf53d3e9dbd4f1f192722dcc7aa1ac4e43a
James P. Fairbanks <fairbanksj@ufl.edu>
2024-06-07 22:11:30 -0400

    ENH: add documentation for AlgebraicPetri Integration

--- ext/AlgebraicDynamicsAlgebraicPetriExt.jl
+++ ext/AlgebraicDynamicsAlgebraicPetriExt.jl
@@ -6,25 +6,33 @@ using Catlab
 using LabelledArrays
 import AlgebraicDynamics.UWDDynam: ContinuousResourceSharer
 
+export OpenNet
+
+"""    OpenNet=Union{OpenPetriNet,OpenLabelledPetriNet,OpenLabelledReactionNet}
+
+A type alias for any kind of open network from AlgebraicPetri.
+"""
+const OpenNet = Union{OpenPetriNet,OpenLabelledPetriNet,OpenLabelledReactionNet}
+
 # vecfields need to be initialized differently depending on type of net
-function vf(pn::OpenPetriNet,T::Type,ns::Int64)
+function dynamics(pn::OpenPetriNet,T::Type,ns::Int64)
   vf(u, p, t) = vectorfield(apex(pn))(zeros(T,ns), u, p, t)
 end
 
-function vf(pn::OpenLabelledPetriNet,T::Type,ns::Int64)
+function dynamics(pn::OpenLabelledPetriNet,T::Type,ns::Int64)
   vf(u, p, t) = vectorfield(apex(pn))(LVector(NamedTuple{tuple(snames(apex(pn))...)}(zeros(T,ns))), u, p, t)
 end
 
-function vf(pn::OpenLabelledReactionNet,T::Type,ns::Int64)
+function dynamics(pn::OpenLabelledReactionNet,T::Type,ns::Int64)
   vf(u, p, t) = vectorfield(apex(pn))(LVector(NamedTuple{tuple(snames(apex(pn))...)}(zeros(T,ns))), u, rates(apex(pn)), t)
 end
 
-function ContinuousResourceSharer{T}(pn::Union{OpenPetriNet,OpenLabelledPetriNet,OpenLabelledReactionNet}) where T
+function ContinuousResourceSharer{T}(pn::OpenNet) where T
   nstates = nparts(apex(pn), :S)
   portmap = vcat(map(legs(pn)) do f
     f[:S](parts(dom(f), :S))
   end...)
   nports = length(portmap)
-  ContinuousResourceSharer{T}(nports, nstates, vf(pn,T,nstates), portmap)
+  ContinuousResourceSharer{T}(nports, nstates, dynamics(pn,T,nstates), portmap)
 end
 end
